5c6933711fb5cb38f7b08e7cf61e209a8cae3fe8
Agrupamento de Escolas Maximinos <aemaximinosweb@gmail.com>
2020-03-09 17:44:39 +0000

    Adicionar arquivos de projeto.

---
+++ Contador.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Contador
+{
+    class Contentor
+    {
+        private string _path = Application.StartupPath + "\\Contador.txt";
+
+        public void contar(object sender)
+        {
+            Button _b = (Button)sender;
+            int valor = Convert.ToInt32(_b.Text) + 1;
+            _b.Text = valor.ToString();
+            File.WriteAllText(_path, valor.ToString());
+
+        }
+
+        public string Abrir()
+        {
+            if (File.Exists(_path)) return File.ReadAllText(_path);
+            else return "0";
+        }
+    }
+}
+
